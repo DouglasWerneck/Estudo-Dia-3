2afad73c704e1458cf4b568c2a06c1deca2ed825
Douglas S Werneck <swdoug_@hotmail.com>
2024-01-18 12:49:39 -0300

    feat: melhorando a calculadora de IMC

--- calculadora IMC/src/Main.java
+++ calculadora IMC/src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Digite sua Altura: ");
         double altura = input.nextDouble();
-        System.out.print("digite o seu Peso: ");
+        System.out.print("Digite o seu Peso: ");
         double peso =  input.nextDouble();
         double resultado = peso / (altura * altura);
-        System.out.println("O seu IMC é " + resultado);
+        String resultadoFormatado = String.format("%.2f", resultado);
+        System.out.println("O seu IMC é " + resultadoFormatado);
         input.close();
+
         if(resultado <= 18.5){
             System.out.println("MAGREZA");
-        } else if (resultado > 18.8  && resultado <= 24.9){
+        } else if (resultado > 18.5 && resultado <= 24.9){
             System.out.println("NORMAL");
         } else if(resultado > 24.9 && resultado <= 29.9){
             System.out.println("SOBREPESO");
-        }else if(resultado > 29.9 && resultado <= 39.9){
+        } else if(resultado > 29.9 && resultado <= 39.9){
             System.out.println("OBESIDADE");
         } else {
             System.out.println("OBESIDADE GRAVE");
